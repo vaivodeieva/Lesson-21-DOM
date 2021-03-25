885acfb7d836b1a67c602fc7c466e2ea61865cf0
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-03-25 21:39:03 +0200

    add solution

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function changeColor() {
+   
+    const h2Element = document.getElementById('important-h2-element');
+    if (h2Element.style.color === 'red') {
+        h2Element.style.color = 'black';
+        h2Element.style.fontSize = '24px';
+        h2Element.innerText = 'Some h2 element';
+
+    } else {
+    h2Element.style.color = 'red';
+    h2Element.style.fontSize = '36px';
+    h2Element.innerText = 'Hello world!';
+}
+
+}
